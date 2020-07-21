459945575c2996946eb785849bd6909afb7d8b06
ghostserverd <42226464+ghostserverd@users.noreply.github.com>
2020-07-20 22:51:36 -0700

    add support for triggering PVR refresh thanks to Michsior14

--- nzbget-postprocess.sh
+++ nzbget-postprocess.sh
@@ -14,6 +14,8 @@
 ARG_PATH="$NZBPP_DIRECTORY"
 ARG_NAME="$NZBPP_NZBNAME"
 ARG_LABEL="$NZBPP_CATEGORY"
+SONARR_CATEGORY=${SONARR_CATEGORY:-"sonarr"}
+RADARR_CATEGORY=${RADARR_CATEGORY:-"radarr"}
 
 # Configuration
 CONFIG_OUTPUT="$HOME/Media"
@@ -28,6 +30,32 @@ echo curl \
 
 echo $FILEBOT_CMD >> /config/filebot.log
 eval $FILEBOT_CMD
+
+REFRESH_NAME=""
+REFRESH_URL=""
+
+case $ARG_LABEL in
+    $SONARR_CATEGORY)
+        REFRESH_NAME="RescanSeries"
+        REFRESH_URL="http://sonarr:${SONARR_PORT}/api/command?apikey=${SONARR_API_KEY}"
+    ;;
+
+    $RADARR_CATEGORY)
+        REFRESH_NAME="RescanMovie"
+        REFRESH_URL="http://radarr:${RADARR_PORT}/api/command?apikey=${RADARR_API_KEY}"
+    ;;
+esac
+
+if [ $REFRESH_URL != "" ]; then
+    REFRESH_CMD=$(\
+        echo curl \
+        -d \"{\\\"name\\\":\\\"${REFRESH_NAME}\\\"}\" \
+        -H \"Content-Type: application/json\" \
+        -X POST \
+        ${REFRESH_URL})
+    echo $REFRESH_CMD >> /config/pvr-refresh.log
+    eval $REFRESH_CMD
+fi
 
 # NZBGet Exit Codes
 if [ $? = 0 ]; then
